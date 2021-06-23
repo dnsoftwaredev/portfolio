8e5b20478d55080faa9ac8b6f3ed36666d95e8a8
Danh <dnsoftwarestore@gmail.com>
2021-06-23 12:05:50 -0500

    migrated from github page to vercel if possible

--- src/components/aboutMe/Landing.js
+++ src/components/aboutMe/Landing.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles((theme) => ({
